5a033a20775d47a394a35aae370ced1064337f40
austinjs <ajsprunger@gmail.com>
2022-06-29 13:25:46 -0600

    addall units to table, reformat date

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ table{
 </style>
 </head>
 <body bgcolor="#EEFDEF">
+  <h1>Testing Table</h1>
 <?php
 
 $url = 'https://api.sightmap.com/v1/assets/1273/multifamily/units';
@@ -24,16 +25,30 @@ curl_setopt($curl, CURLOPT_HTTPHEADER, [
   'Content-Type: application/json'
 ]);
 
-$response = curl_exec($curl);
+$response = json_decode(curl_exec($curl));
 curl_close($curl);
 
 // echo $response . PHP_EOL;
 
-while($response) {
-  echo "<tr>";
-  echo "<td>".$response["id"]."</td>";
-  echo"</td>";
-}
+// while($response) {
+//   echo "<tr>";
+//   echo "<td>".$response["id"]."</td>";
+//   echo "</td>";
+// }
+
+echo "<table>";
+echo "<th> Unit Number </th>";
+echo "<th> Area </th>";
+echo "<th> Updated At </th>";
+  foreach ($response->data as $unit) {
+    // echo $unit->id;
+    echo "<tr>";
+      echo "<td>".$unit->unit_number."</td>";
+      echo "<td>".$unit->area."</td>";
+      echo "<td>".date("m-d-y, g:i", $unit->updated_at)."</td>";
+    echo "</tr>";
+  }
+echo "</table>";
 
 ?>
 </body>
